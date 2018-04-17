195100891dc085221c61ec97a2c571e38a4ae384
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2018-04-17 22:58:34 +0700

    Add addingPreset layout

---
+++ app/src/main/java/com/hundredacrewoods/pomodoroandroid/fragments/AddingPresetFragment.java
@@ -0,0 +1,179 @@
+package com.hundredacrewoods.pomodoroandroid.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.hundredacrewoods.pomodoroandroid.R;
+
+public class AddingPresetFragment extends Fragment {
+
+    private TextView mFocusTimeTextView;
+
+    private TextView mShortBreakTimeTextView;
+
+    private TextView mLongBreakTimeTextView;
+
+    private SeekBar mFocusTimeSeekBar;
+
+    private SeekBar mShortBreakTimeSeekBar;
+
+    private SeekBar mLongBreakTimeSeekBar;
+
+    private Button mSavePresetButton;
+
+    public AddingPresetFragment () {
+        super();
+    }
+
+    @SuppressWarnings("unused")
+    public static AddingPresetFragment newInstance() {
+        AddingPresetFragment fragment = new AddingPresetFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init(savedInstanceState);
+
+        if (savedInstanceState != null)
+            onRestoreInstanceState(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_adding_preset, container,
+                false);
+        initInstances(rootView, savedInstanceState);
+
+        mFocusTimeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                i = i * 5 + 15;
+                String text = String.valueOf(i) + " minutes";
+                mFocusTimeTextView.setText(text);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mShortBreakTimeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                i = i + 1;
+                String text = String.valueOf(i) + " minutes";
+                mShortBreakTimeTextView.setText(text);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mLongBreakTimeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                i = i * 3 + 5;
+                String text = String.valueOf(i) + " minutes";
+                mLongBreakTimeTextView.setText(text);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mSavePresetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment editPresetNameFragment = new editPresetNameFragment();
+                FragmentManager fragmentManager = AddingPresetFragment.super.getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(editPresetNameFragment, null);
+//                fragmentTransaction.replace(R.id.frame_fragmentholder,
+//                        addingPresetFragment);
+                fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return rootView;
+    }
+
+    private void init(Bundle savedInstanceState) {
+        // Init Fragment level's variable(s) here
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private void initInstances(View rootView, Bundle savedInstanceState) {
+        // Init 'View' instance(s) with rootView.findViewById here
+        mFocusTimeSeekBar = rootView.findViewById(R.id.focusTimeSlider);
+        mFocusTimeTextView = rootView.findViewById(R.id.focusTimeText);
+        mShortBreakTimeSeekBar = rootView.findViewById(R.id.shortBreakTimeSlider);
+        mShortBreakTimeTextView = rootView.findViewById(R.id.shortBreakTimeText);
+        mLongBreakTimeSeekBar = rootView.findViewById(R.id.longBreakTimeSlider);
+        mLongBreakTimeTextView = rootView.findViewById(R.id.longBreakTimeText);
+        mSavePresetButton = rootView.findViewById(R.id.savePresetButton);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    /*
+     * Save Instance State Here
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save Instance State here
+    }
+
+    /*
+     * Restore Instance State Here
+     */
+    @SuppressWarnings("UnusedParameters")
+    private void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore Instance State here
+    }
+}
--- app/src/main/java/com/hundredacrewoods/pomodoroandroid/fragments/editPresetNameFragment.java
+++ app/src/main/java/com/hundredacrewoods/pomodoroandroid/fragments/editPresetNameFragment.java
@@ -1,41 +1,29 @@
 package com.hundredacrewoods.pomodoroandroid.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.SeekBar;
 
 import com.hundredacrewoods.pomodoroandroid.R;
 
-/**
- * Created by nuuneoi on 11/16/2014.
- */
-@SuppressWarnings("unused")
-public class PresetFragment extends Fragment {
+public class editPresetNameFragment extends DialogFragment {
 
-    private FloatingActionButton mFloatingActionButton;
-
-    public PresetFragment() {
+    public editPresetNameFragment() {
         super();
     }
 
     @SuppressWarnings("unused")
-    public static PresetFragment newInstance() {
-        PresetFragment fragment = new PresetFragment();
+    public static editPresetNameFragment newInstance() {
+        editPresetNameFragment fragment = new editPresetNameFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         init(savedInstanceState);
@@ -48,21 +36,10 @@ public class PresetFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_preset, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_adding_preset, container,
+                false);
         initInstances(rootView, savedInstanceState);
-        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Fragment addingPresetFragment = new AddingPresetFragment();
-                FragmentManager fragmentManager = PresetFragment.super.getFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.frame_fragmentholder,
-                        addingPresetFragment);
-                fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
-            }
-        });
+
         return rootView;
     }
 
@@ -73,7 +50,7 @@ public class PresetFragment extends Fragment {
     @SuppressWarnings("UnusedParameters")
     private void initInstances(View rootView, Bundle savedInstanceState) {
         // Init 'View' instance(s) with rootView.findViewById here
-        mFloatingActionButton = rootView.findViewById(R.id.fragment_preset_add_button);
+
     }
 
     @Override
@@ -102,5 +79,4 @@ public class PresetFragment extends Fragment {
     private void onRestoreInstanceState(Bundle savedInstanceState) {
         // Restore Instance State here
     }
-
 }
