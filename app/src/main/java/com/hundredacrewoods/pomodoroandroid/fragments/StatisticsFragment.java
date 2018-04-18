d5a15035f824b8cb30f999047cd71d29144311a6
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2018-04-18 15:27:15 +0700

    Add BarChart

--- app/src/main/java/com/hundredacrewoods/pomodoroandroid/fragments/StatisticsFragment.java
+++ app/src/main/java/com/hundredacrewoods/pomodoroandroid/fragments/StatisticsFragment.java
@@ -1,5 +1,6 @@
 package com.hundredacrewoods.pomodoroandroid.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,7 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.hundredacrewoods.pomodoroandroid.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by nuuneoi on 11/16/2014.
@@ -16,7 +26,7 @@ import com.hundredacrewoods.pomodoroandroid.R;
 @SuppressWarnings("unused")
 public class StatisticsFragment extends Fragment {
 
-    TextView textView;
+    private BarChart mBarChart;
 
     public StatisticsFragment() {
         super();
@@ -44,6 +54,56 @@ public class StatisticsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_statistics, container, false);
         initInstances(rootView, savedInstanceState);
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, 10f));
+        entries.add(new BarEntry(1f, 2f));
+        entries.add(new BarEntry(2f, 7f));
+        entries.add(new BarEntry(3f, 12f));
+        entries.add(new BarEntry(5f, 14f));
+        entries.add(new BarEntry(6f, 4f));
+        entries.add(new BarEntry(4f, 2f));
+
+        BarDataSet set = new BarDataSet(entries, "Success Pomodoro");
+        set.setColors(Color.GRAY);
+
+        //Add data, failed pomodoro
+        List<BarEntry> entriesGroup2 = new ArrayList<>();
+        entriesGroup2.add(new BarEntry(0f,2f));
+        entriesGroup2.add(new BarEntry(1f,5f));
+        entriesGroup2.add(new BarEntry(2f,1f));
+        entriesGroup2.add(new BarEntry(3f,0f));
+        entriesGroup2.add(new BarEntry(4f,6f));
+        entriesGroup2.add(new BarEntry(5f,3f));
+        entriesGroup2.add(new BarEntry(6f,4f));
+
+        BarDataSet set2 = new BarDataSet(entriesGroup2, "Failed Pomodoro");
+        set2.setColor(Color.YELLOW);
+
+        List<String> BarEntryLabels = new ArrayList<String>();
+        BarEntryLabels.add("Mon");
+        BarEntryLabels.add("Tue");
+        BarEntryLabels.add("Wed");
+        BarEntryLabels.add("Thu");
+        BarEntryLabels.add("Fri");
+        BarEntryLabels.add("Sat");
+        BarEntryLabels.add("Sun");
+
+        BarData data = new BarData(set, set2);
+        data.setBarWidth(0.6f); // set custom bar width
+        mBarChart.setData(data);
+        //chart.groupBars(0.00001f, 0.2f, 0.02f);
+        //chart.setFitBars(true); // make the x-axis fit exactly all bars
+        mBarChart.invalidate(); // refresh
+        mBarChart.animateXY(3000,3000);
+
+        mBarChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(BarEntryLabels));
+        mBarChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        mBarChart.getAxisRight().setEnabled(false);
+        mBarChart.getAxisLeft().setDrawGridLines(false);
+        mBarChart.getXAxis().setDrawGridLines(false);
+        mBarChart.getDescription().setEnabled(false);
+        //chart.getXAxis().setCenterAxisLabels(true);
+        mBarChart.setDragEnabled(true);
         return rootView;
     }
 
@@ -54,7 +114,7 @@ public class StatisticsFragment extends Fragment {
     @SuppressWarnings("UnusedParameters")
     private void initInstances(View rootView, Bundle savedInstanceState) {
         // Init 'View' instance(s) with rootView.findViewById here
-        textView = rootView.findViewById(R.id.fragment_statistics_textview);
+        mBarChart = (BarChart) rootView.findViewById(R.id.chart);
     }
 
     @Override
