fab5bddc0167dd3ac1ba7b6edc15fa0f5c33f459
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2018-04-19 14:18:11 +0700

    Add PomodoroViewModel

---
+++ app/src/main/java/com/hundredacrewoods/pomodoroandroid/databases/PomodoroViewModel.java
@@ -0,0 +1,28 @@
+package com.hundredacrewoods.pomodoroandroid.databases;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+public class PomodoroViewModel extends AndroidViewModel {
+
+    private PomodoroRepository mPomodoroRepository;
+
+    private LiveData<List<Preset>> mAllPresets;
+
+    public PomodoroViewModel (Application application) {
+        super(application);
+        mPomodoroRepository = new PomodoroRepository(application);
+        mAllPresets = mPomodoroRepository.getAllPresets();
+    }
+
+    public LiveData<List<Preset>> getmAllPresets() {
+        return mAllPresets;
+    }
+
+    public void insert(Preset... presets) {
+        mPomodoroRepository.insert(presets);
+    }
+}
