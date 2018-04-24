142801bdb43398d3eecc33c32f51d76c0ade56d2
JIMMYSURFACE\jimmy <sobearfinn@gmail.com>
2018-04-24 09:48:44 +0700

    Add timer pause function

--- app/src/main/java/com/hundredacrewoods/pomodoroandroid/fragments/TimerFragment.java
+++ app/src/main/java/com/hundredacrewoods/pomodoroandroid/fragments/TimerFragment.java
@@ -13,6 +13,9 @@ import android.widget.TextView;
 
 import com.hundredacrewoods.pomodoroandroid.R;
 
+import java.util.Locale;
+import java.util.Timer;
+
 /**
  * Created by nuuneoi on 11/16/2014.
  */
@@ -22,12 +25,24 @@ public class TimerFragment extends Fragment {
     int shortBreakTime;
     int longBreakTime;
     int focusTime;
-    int currentStatus;
-    int shortBreakCount;
     int shortBreakPerLongBreak;
+
+    public enum TimerStatus {
+        FOCUS, LONG_BREAK, SHORT_BREAK
+    }
+
+    CountDownTimer countDownTimer;
+    TimerStatus currentStatus;
+    long currentTimeLeftInMillis;
+    int shortBreakCount;
+
+    boolean isTimerRunning;
+
     TextView presetNameTextView;
     TextView timerTextView;
     Button startButton;
+    Button pauseButton;
+    Button stopButton;
 
     public TimerFragment() {
         super();
@@ -60,12 +75,16 @@ public class TimerFragment extends Fragment {
 
     private void init(Bundle savedInstanceState) {
         // Init Fragment level's variable(s) here
-        focusTime = 20;
-        shortBreakTime = 5;
-        longBreakTime = 15;
-        shortBreakCount = 0;
-        currentStatus = 0;
+        focusTime = 20000;
+        shortBreakTime = 5000;
+        longBreakTime = 15000;
         shortBreakPerLongBreak = 1;
+
+        shortBreakCount = 0;
+        currentTimeLeftInMillis = focusTime;
+        currentStatus = TimerStatus.FOCUS;
+
+        isTimerRunning = false;
     }
 
     @SuppressWarnings("UnusedParameters")
@@ -77,58 +96,103 @@ public class TimerFragment extends Fragment {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startTimer();
+                startButtonPressed();
             }
         });
+        pauseButton = rootView.findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+        });
+        stopButton = rootView.findViewById(R.id.stop_button);
+    }
+
+    public void startButtonPressed() {
+        if (isTimerRunning) {
+            Log.d("startButtonPressed", "pausing timer...");
+            pauseTimer();
+        } else {
+            Log.d("startButtonPressed", "starting timer...");
+            startTimer();
+        }
+        isTimerRunning = !isTimerRunning;
+    }
+
+    public void pauseTimer() {
+        countDownTimer.cancel();
+        this.startButton.setText("Continue");
     }
 
     public void startTimer() {
-        Log.i("Timer", "Button clicked!");
         int secondsInFuture = 0;
-        switch (currentStatus) {
-            case 0:
-                secondsInFuture = focusTime;
-                presetNameTextView.setText(R.string.focus_text);
-                break;
-            case 1:
-                secondsInFuture = shortBreakTime;
-                presetNameTextView.setText(R.string.short_break_text);
-                break;
-            case 2:
-                secondsInFuture = longBreakTime;
-                presetNameTextView.setText(R.string.long_break_text);
-                break;
-            default:
-                break;
-        }
-        new CountDownTimer(secondsInFuture * 1000, 1000) {
+        this.startButton.setText("Pause");
+
+        countDownTimer = new CountDownTimer(currentTimeLeftInMillis, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                long minutesLeft = (millisUntilFinished / 1000) / 60;
-                long secondsLeft = (millisUntilFinished / 1000) % 60;
-                String timeLeft = minutesLeft + ":" + secondsLeft;
-                timerTextView.setText(timeLeft);
+                currentTimeLeftInMillis = millisUntilFinished;
+                updateTimerText();
             }
 
             public void onFinish() {
-                switch (currentStatus) {
-                    case 0:
-                        if (shortBreakCount > shortBreakPerLongBreak) {
-                            currentStatus = 2;
-                            shortBreakCount = 0;
-                        } else {
-                            currentStatus = 1;
-                            shortBreakCount++;
-                        }
-                        startTimer();
-                        break;
-                    default:
-                        currentStatus = 0;
-                        startTimer();
-                }
+                changeTimerStatus();
                 timerTextView.setText(R.string.timer_text);
+                startTimer();
             }
         }.start();
+
+        updateTimerText();
+    }
+
+    public void resetTimer() {
+        countDownTimer.cancel();
+        setTimerStatusParams(TimerStatus.FOCUS);
+        shortBreakCount = 0;
+        isTimerRunning = false;
+    }
+
+    void updateTimerText() {
+        long minutesLeft = (currentTimeLeftInMillis / 1000) / 60;
+        long secondsLeft = (currentTimeLeftInMillis / 1000) % 60;
+        timerTextView.setText(String.format(Locale.getDefault(), "%d:%02d", minutesLeft, secondsLeft));
+    }
+
+    void changeTimerStatus() {
+        switch (currentStatus) {
+            case FOCUS:
+                if (shortBreakCount > shortBreakPerLongBreak) {
+                    setTimerStatusParams(TimerStatus.LONG_BREAK);
+                    shortBreakCount = 0;
+                } else {
+                    setTimerStatusParams(TimerStatus.SHORT_BREAK);
+                    shortBreakCount++;
+                }
+                break;
+            default:
+                setTimerStatusParams(TimerStatus.FOCUS);
+        }
+    }
+
+    void setTimerStatusParams(TimerStatus timerStatus) {
+        switch (timerStatus) {
+            case FOCUS:
+                setTimerStatusParams(timerStatus, focusTime, R.string.focus_text);
+                break;
+            case SHORT_BREAK:
+                setTimerStatusParams(timerStatus, shortBreakTime, R.string.short_break_text);
+                break;
+            case LONG_BREAK:
+                setTimerStatusParams(timerStatus, longBreakTime, R.string.long_break_text);
+                break;
+        }
+    }
+
+    void setTimerStatusParams(TimerStatus timerStatus, long timeInMillis, int stringResId) {
+        currentStatus = timerStatus;
+        currentTimeLeftInMillis = timeInMillis;
+        presetNameTextView.setText(stringResId);
     }
 
     @Override
