9736ce95d67b3e8bbaeac9ac6e8737076302fd83
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2018-04-29 00:06:23 +0700

    Add db connection to month stats

--- app/src/main/java/com/hundredacrewoods/pomodoroandroid/fragments/StatisticsFragmentMonth.java
+++ app/src/main/java/com/hundredacrewoods/pomodoroandroid/fragments/StatisticsFragmentMonth.java
@@ -7,25 +7,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.hundredacrewoods.pomodoroandroid.R;
+import com.hundredacrewoods.pomodoroandroid.TimestampRange;
+import com.hundredacrewoods.pomodoroandroid.activities.MainActivity;
+import com.hundredacrewoods.pomodoroandroid.databases.PomodoroViewModel;
+import com.hundredacrewoods.pomodoroandroid.databases.UserRecord;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StatisticsFragmentMonth extends Fragment {
 
-    private LineChart monthLineChart;
-    List<Entry> successPomo;
-    List<Entry> failedPomo;
+    private BarChart mBarChart;
+    List<BarEntry> successPomo;
+    List<BarEntry> failedPomo;
     List<String> monthLabels;
+
+    private PomodoroViewModel mPomodoroViewModel;
 
     public StatisticsFragmentMonth(){
 
@@ -33,93 +46,104 @@ public class StatisticsFragmentMonth extends Fragment {
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        mPomodoroViewModel = ((MainActivity) getActivity()).getPomodoroViewModel();
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.fragment_statistics_month, container, false);
-        monthLineChart = rootView.findViewById(R.id.month_linechart);
-        loadData();
-        addMonthLabel();
-        setDataOnLineChart();
-        setCustomizationOnBarChart();
+        mBarChart = rootView.findViewById(R.id.month_barchart);
+
+        mPomodoroViewModel.selectUserRecords().observe(getActivity(), userRecords -> {
+            loadData(userRecords);
+            addMonthLabel();
+            setDataOnBarChart();
+            setCustomizationOnBarChart();
+        });
+
+        mPomodoroViewModel.setFilterSearch(TimestampRange.getTimestampRange(
+                TimestampRange.THIS_MONTH, new Timestamp(System.currentTimeMillis())));
+
         return rootView;
     }
 
-    public void loadData(){
+    public void loadData(List<UserRecord> userRecords) {
         successPomo = new ArrayList<>();
-        // x-axis = month
-        // x=0 is equal to Jan
-        // x=12 is equal to Dec
-        successPomo.add(new Entry(0f, 10f));
-        successPomo.add(new Entry(1f, 2f));
-        successPomo.add(new Entry(2f, 7f));
-        successPomo.add(new Entry(3f, 12f));
-        successPomo.add(new Entry(5f, 14f));
-        successPomo.add(new Entry(6f, 4f));
-        successPomo.add(new Entry(4f, 2f));
-
         failedPomo = new ArrayList<>();
-        failedPomo.add(new Entry(0f,2f));
-        failedPomo.add(new Entry(1f,5f));
-        failedPomo.add(new Entry(2f,1f));
-        failedPomo.add(new Entry(3f,0f));
-        failedPomo.add(new Entry(4f,6f));
-        failedPomo.add(new Entry(5f,3f));
-        failedPomo.add(new Entry(6f,4f));
+
+        Calendar calendar = Calendar.getInstance();
+
+        userRecords.stream().collect(Collectors.groupingBy(userRecord -> {
+            calendar.setTimeInMillis(userRecord.getStartDateTime().getTime());
+            return calendar.get(Calendar.WEEK_OF_MONTH);
+        }, Collectors.summingInt(userRecord -> userRecord.getNumSuccess())))
+                .forEach((week, sumSuccess) -> successPomo.add(new BarEntry((float) week, (float) sumSuccess)));
+
+        userRecords.stream().collect(Collectors.groupingBy(userRecord -> {
+            calendar.setTimeInMillis(userRecord.getStartDateTime().getTime());
+            return calendar.get(Calendar.WEEK_OF_MONTH);
+        }, Collectors.summingInt(userRecord -> userRecord.getNumFailure())))
+                .forEach((week, sumFailure) -> failedPomo.add(new BarEntry((float) week, (float) sumFailure)));
+    }
+
+    public void loadData(){
+//        successPomo = new ArrayList<>();
+//        // x-axis = month
+//        // x=0 is equal to Jan
+//        // x=12 is equal to Dec
+//        successPomo.add(new Entry(0f, 10f));
+//        successPomo.add(new Entry(1f, 2f));
+//        successPomo.add(new Entry(2f, 7f));
+//        successPomo.add(new Entry(3f, 12f));
+//        successPomo.add(new Entry(5f, 14f));
+//        successPomo.add(new Entry(6f, 4f));
+//        successPomo.add(new Entry(4f, 2f));
+//
+//        failedPomo = new ArrayList<>();
+//        failedPomo.add(new Entry(0f,2f));
+//        failedPomo.add(new Entry(1f,5f));
+//        failedPomo.add(new Entry(2f,1f));
+//        failedPomo.add(new Entry(3f,0f));
+//        failedPomo.add(new Entry(4f,6f));
+//        failedPomo.add(new Entry(5f,3f));
+//        failedPomo.add(new Entry(6f,4f));
     }
 
     public void addMonthLabel(){
         monthLabels = new ArrayList<>();
-        monthLabels.add("Jan");
-        monthLabels.add("Feb");
-        monthLabels.add("Mar");
-        monthLabels.add("Apr");
-        monthLabels.add("May");
-        monthLabels.add("Jun");
-        monthLabels.add("Jul");
-        monthLabels.add("Aug");
-        monthLabels.add("Sep");
-        monthLabels.add("Oct");
-        monthLabels.add("Nov");
-        monthLabels.add("Dec");
-
+        monthLabels.add("Week 1");
+        monthLabels.add("Week 2");
+        monthLabels.add("Week 3");
+        monthLabels.add("Week 4");
+        monthLabels.add("Week 5");
     }
 
-    public void setDataOnLineChart(){
-        LineDataSet dataSet1 = new LineDataSet(successPomo, "Success Pomodoro");
-        dataSet1.setColor(Color.rgb(255,193,7));
-        dataSet1.setAxisDependency(YAxis.AxisDependency.LEFT);
-        dataSet1.setLineWidth(3f);
-        dataSet1.setCircleRadius(10f);
-        dataSet1.setCircleColor(Color.rgb(255,87,34));
+    public void setDataOnBarChart(){
+        BarDataSet set = new BarDataSet(successPomo, "Success Pomodoro");
+        set.setColors(Color.rgb(255,193,7));
 
-        LineDataSet dataSet2 = new LineDataSet(failedPomo, "Failed Pomodoro");
-        dataSet2.setColor(Color.rgb(117,117,177));
-        dataSet2.setAxisDependency(YAxis.AxisDependency.LEFT);
-        dataSet2.setLineWidth(3f);
-        dataSet2.setCircleRadius(10f);
-        dataSet2.setCircleColor(Color.rgb(255,87,34));
+        BarDataSet set2 = new BarDataSet(failedPomo, "Failed Pomodoro");
+        set2.setColor(Color.rgb(189,189,189));
 
-        List<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
-        dataSets.add(dataSet1);
-        dataSets.add(dataSet2);
-
-        LineData data = new LineData(dataSets);
-        monthLineChart.setData(data);
-        monthLineChart.invalidate(); // refresh
+        BarData data = new BarData(set, set2);
+        data.setBarWidth(0.6f); // set custom bar width
+        mBarChart.setData(data);
     }
 
     public void setCustomizationOnBarChart(){
-        monthLineChart.animateXY(1000, 1000);
-        monthLineChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(monthLabels));
-        monthLineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-        monthLineChart.getAxisRight().setEnabled(false);
-        monthLineChart.getAxisLeft().setDrawGridLines(false);
-        monthLineChart.getXAxis().setDrawGridLines(false);
-        monthLineChart.getDescription().setEnabled(false);
-        monthLineChart.setVisibleXRangeMaximum(6);
-        monthLineChart.moveViewToX(6);
-        monthLineChart.setScaleEnabled(false);
+        //chart.groupBars(0.00001f, 0.2f, 0.02f);
+        //chart.setFitBars(true); // make the x-axis fit exactly all bars
+        mBarChart.animateXY(1000,1000);
+
+        mBarChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(monthLabels));
+        mBarChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        mBarChart.getAxisRight().setEnabled(false);
+        mBarChart.getAxisLeft().setDrawGridLines(false);
+        mBarChart.getXAxis().setDrawGridLines(false);
+        mBarChart.getDescription().setEnabled(false);
+        //chart.getXAxis().setCenterAxisLabels(true);
+        mBarChart.setDragEnabled(true);
+        mBarChart.setScaleEnabled(false);
+        mBarChart.invalidate(); // refresh
     }
 
 }
